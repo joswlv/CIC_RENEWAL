20526944097ad0949ae71cd5bbe10e6291a02dfd
Mr.Jo <wh7923@naver.com>
2015-03-17 07:27:24 +0000

    git_test

--- app/controllers/useradd_controller.rb
+++ app/controllers/useradd_controller.rb
@@ -3,6 +3,21 @@ class UseraddController < ApplicationController
   end
 
   def signup_complete
+    user = HomeMember.new
+    user.name = params[:username]
+    if params[:password] == params[:retype_password]
+      user.password = params[:password]
+      if user.save
+        flash[:alert] = "성공으로 가입됬습니다."
+        redirect_to "/"
+      else
+        flash[:alert] = user.errors.values.flatten.join(' ')
+        redirect_to :back
+      end
+      else
+        flash[:alert]="비밀번호가 맞지 않습니다."
+        redirect_to :back
+    end
   end
 
   def login
